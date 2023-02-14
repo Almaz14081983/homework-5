6cd50f9075be06c7f504aa82b9687d06af9add01
Almaz <minnexanov.a@mail.ru>
2023-02-14 11:25:55 +0300

    seminar5DZTask38b

---
+++ seminar5DZTask38b/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.Clear();
+
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine()!);
+double[] array = GetArray(size);
+Console.WriteLine("сгенерированный массив из случайных чисел: [" + String.Join(", ", array) + "],");
+Console.WriteLine($"разницу между максимальным и минимальным элементами массива: {ChangeSign(array)}.");
+
+double[] GetArray(int size)
+{
+    double[] res = new double [size];
+    for(int i=0; i<size; i++)
+    {
+        res[i] = new Random().NextDouble() + new Random().Next(-100, 100);       
+    }
+    return res;    
+}
+
+double ChangeSign(double[] someArray)
+{
+    double maxindex = someArray[0];
+    double minindex = someArray[0];
+    foreach( var index in someArray)
+    {
+        if(maxindex < index) maxindex = index;
+        if(minindex > index) minindex = index;      
+    }
+    var result = maxindex - minindex;
+ 
+    Console.WriteLine($"из них максимальное число: {maxindex},");
+    Console.WriteLine($"из них минимальное число: {minindex},");
+    
+    return result;          
+}
+
+
